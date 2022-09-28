2256ff4262d915b3085ceb83a21e9bb1481e563a
codewithJosh <dacasinjoshua21@gmail.com>
2022-09-28 15:25:52 +0800

    feat: add `Player Camera` control

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public Transform player;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float lookX = SimpleInput.GetAxis("LookX") * mouseSensitivity * Time.deltaTime;
+        float lookY = SimpleInput.GetAxis("LookY") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * lookX);
+
+    }
+
+}
