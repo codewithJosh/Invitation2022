87676d6e52e20d1b3bdcf3beb483b5748b37298b
codewithJosh <dacasinjoshua21@gmail.com>
2022-09-30 00:08:57 +0800

    feat: add new `Player Camera` control

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -7,7 +7,7 @@ public class PlayerCamera : MonoBehaviour
 
     public float mouseSensitivity = 100f;
     public Transform player;
-    float xRotation = 0f;
+    float xRotation = 20f;
 
     // Start is called before the first frame update
     void Start()
