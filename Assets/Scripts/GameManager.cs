4d47e5253a2a169c2563873121e21539d473ea52
codewithJosh <dacasinjoshua21@gmail.com>
2022-10-03 00:07:56 +0800

    feat: add `Start Menu` animation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Animator animator;
+    private enum startMenuState { idle, start, help, about, quit };
+    private startMenuState state = startMenuState.idle;
+
+    void Update()
+    {
+
+        if (SimpleInput.GetButtonDown("OnStartButton"))
+        {
+
+            state = startMenuState.start;
+
+        }
+        if (SimpleInput.GetButtonDown("OnHelpButton"))
+        {
+
+            state = startMenuState.help;
+
+        }
+        if (SimpleInput.GetButtonDown("OnAboutButton"))
+        {
+
+            state = startMenuState.about;
+
+        }
+        if (SimpleInput.GetButtonDown("OnQuitButton"))
+        {
+
+            state = startMenuState.quit;
+
+        }
+
+        animator.SetInteger("state", (int) state);
+
+    }
+
+}
