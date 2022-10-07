bf46ce521fd49f7275bf134cb1806158ffc45040
codewithJosh <dacasinjoshua21@gmail.com>
2022-10-07 10:38:26 +0800

    feat: add new `Premonition` scene

--- Assets/Scripts/LaunchManager.cs
+++ Assets/Scripts/LaunchManager.cs
@@ -1,35 +1,103 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LaunchManager : MonoBehaviour
 {
 
-    private float timeDelayed;
-    private float timeElapsed;
+    [SerializeField] private Animator animator;
+
+    private enum premonitionStates { idle, premonition, creditsLight, creditsDark };
+    private premonitionStates premonitionState = premonitionStates.idle;
+
+    private bool canTap;
 
     void Start()
     {
 
-        timeDelayed = 50f;
+        canTap = false;
+        int countdown = 50;
+        StartCoroutine(PremonitionToStart(countdown));
 
     }
 
-    void Update()
+    IEnumerator PremonitionToStart(int _countdown)
     {
 
-        timeElapsed += Time.deltaTime;
+        premonitionState = premonitionStates.premonition;
+        animator.SetInteger("premonitionState", (int) premonitionState);
 
-        if (timeElapsed > timeDelayed)
+        int count = 0;
+
+        while (_countdown > 0)
         {
 
-            OnTapToSkip();
+            if (count == 4 && canTap == false)
+            {
+
+                canTap = true;
+
+            }
+            else if (count < 5)
+            {
+
+                count++;
+
+            }
+
+            yield return new WaitForSeconds(1f);
+
+            _countdown--;
 
         }
+
+        OnTapToSkip();
 
     }
 
     public void OnTapToSkip()
     {
+
+        if (canTap == true)
+        {
+
+            int countdown = 5;
+            StartCoroutine(CreditsToStart(countdown));
+
+        }
+
+    }
+
+    IEnumerator CreditsToStart(int _countdown)
+    {
+
+        int x = Random.Range(0, 2);
+
+        if (x == 0)
+        {
+
+            premonitionState = premonitionStates.creditsLight;
+
+        }
+        else if (x == 1)
+        {
+
+            premonitionState = premonitionStates.creditsDark;
+
+        }
+
+        animator.SetInteger("premonitionState", (int)premonitionState);
+
+        while (_countdown > 0)
+        {
+
+            yield return new WaitForSeconds(1f);
+
+            _countdown--;
+
+        }
+
+        yield return new WaitForSeconds(1f);
 
         SceneManager.LoadScene(1);
 
